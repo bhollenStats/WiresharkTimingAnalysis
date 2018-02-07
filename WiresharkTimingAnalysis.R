a54e2c003ea66c40e098047ce65468d58a860544
Unknown <bhollenStats@gmail.com>
2018-02-06 19:58:11 -0500

    Small  Tweaks
    Small tweaks to make it easier to reuse the code with only a few small changes for the additional CSVs to analyze

--- WiresharkTimingAnalysis.R
+++ WiresharkTimingAnalysis.R
@@ -39,29 +39,60 @@ library(ggplot2)
 
 # Variables
 debug <- TRUE
+xmitPacketDissectionFilename <- './AWRT_Transmit_PacketDissections.csv'
+recvPacketDissectionFilename <- './AWRT_Receive_PacketDissections.csv'
+# xmitPacketDissectionFilename <- './AWRT_Transmit_PacketDissections_Test2.csv', sep = ',')
+# recvPacketDissectionFilename <- './AWRT_Receive_PacketDissections_Test2.csv', sep = ',')
+
+# Utility Functions
+dataDump <- function(data) {
+  m_tdt <- mean(data)
+  s_tdt <- sd(data)
+  N_tdt <- length(data)
+  e_tdt <- qnorm(0.025, lower.tail = FALSE * s_tdt / sqrt(N_tdt))
+  lower <- m_tdt - e_tdt
+  upper <- m_tdt + e_tdt
+  
+  cat('\nData Dump Results')
+  cat('\n')
+  cat('\n  Mean  = ', m_tdt)
+  cat('\n  Sd    = ', s_tdt)
+  cat('\n  N     = ', N_tdt)
+  cat('\n  Error = ', e_tdt)
+  cat('\n  Lower = ', lower)
+  cat('\n  Upper = ', upper)
+  cat('\n  Min   = ', min(data))
+  cat('\n  Max   = ', max(data))
+}
 
 # Input the two packet dissection files from WireShark
-xmitPackets <- read.csv(file = './AWRT_Transmit_PacketDissections.csv', sep = ',')
+# Expected column names are:
+#   [1] "No.","Time","Source","Destination","Protocol","Length","Info"  
+xmitPackets <- read.csv(file = xmitPacketDissectionFilename)
 if(debug) head(xmitPackets)
-recvPackets <- read.csv(file = './AWRT_Receive_PacketDissections.csv', sep = ',')
+recvPackets <- read.csv(file = recvPacketDissectionFilename)
 if(debug) head(recvPackets)
 
 # Now clean up the data so that they can be joined. I've assumed that each transmit line will match
 # to a subsequent response line, so I plan to join the data based on that online 'transaction.' 
 # With this resulting table I can calculate the time different (deltaT) between the transmitted
-# command and the response from the device
+# command and the response from the device.
+#
+# xmitData column names should be "PacketNo","XmitSeqNo","XmitTimeMs"
 xmitData <- xmitPackets %>%
   mutate(PacketNo = row_number(), XmitTimeMs = Time * 1000) %>%
   rename(XmitSeqNo = 'No.') %>%
   select(PacketNo, XmitSeqNo, XmitTimeMs)
 if(debug) head(xmitData)
 
+# recvData column names should be "PacketNo","RecvSeqNo","RecvTimeMs"
 recvData <- recvPackets %>%
   mutate(PacketNo = row_number(), RecvTimeMs = Time * 1000) %>%
   rename(RecvSeqNo = 'No.') %>%
   select(PacketNo, RecvSeqNo, RecvTimeMs)
 if(debug) head(recvData)
 
+# "joined" transactionData column names should be "PacketNo","XmitSeqNo","RecvSeqNo","XmitTimeMs","RecvTimeMs","deltaTms"
 transactionData <- xmitData %>%
   inner_join(recvData, by = 'PacketNo') %>%
   mutate(deltaTms = RecvTimeMs - XmitTimeMs) %>%
@@ -69,30 +100,14 @@ transactionData <- xmitData %>%
 if(debug) head(transactionData)
 
 # Evaluation of the timing between successive online command requests
+# transmitDeltaT column names should be "PacketNo","XmitSeqNo","XmitTimeMs"
 transmitDeltaT <- tail(xmitData, -1) - head(xmitData, -1)
-if(debug) head(transmitDeltaT)
-
-m_tdt <- mean(transmitDeltaT$XmitTimeMs)
-s_tdt <- sd(transmitDeltaT$XmitTimeMs)
-N_tdt <- length(transmitDeltaT$XmitTimeMs)
-e_tdt <- qnorm(0.025, lower.tail = FALSE * s_tdt / sqrt(N_tdt))
-lower_transmitDeltaT <- m_tdt - e_tdt
-upper_transmitDeltaT <- m_tdt + e_tdt
-if(debug) {
-  cat('\nTranmission DeltaT Results')
-  cat('\nMean  = ', m_tdt)
-  cat('\nSd    = ', s_tdt)
-  cat('\nN     = ', N_tdt)
-  cat('\nError = ', e_tdt)
-  cat('\nLower = ', lower_transmitDeltaT)
-  cat('\nUpper = ', upper_transmitDeltaT)
-}
+if(debug) dataDump(transmitDeltaT$XmitTimeMs)
 
 transmitDeltaT %>%
   ggplot(aes(x=XmitTimeMs)) +
   geom_histogram(binwidth = 0.02, color = 'darkgreen', fill = 'lightgreen') + 
-  #coord_cartesian(xlim = c(99,101)) + 
-  coord_cartesian(xlim = c(lower_transmitDeltaT, upper_transmitDeltaT)) +
+  coord_cartesian(xlim = c(99,101)) + 
   labs(x = '[ms]',
        y = '',
        title = 'Time Difference Distribution',
@@ -100,31 +115,16 @@ transmitDeltaT %>%
   theme_dark()
 
 # Evaluation of the response time from the device to the request of online data
+# onlineResponseTime column names should be "PacketNo","deltaTms"
 onlineResponseTimes <- transactionData %>%
-  select(deltaTms) %>%
-  filter(deltaTms <= 5)
-if(debug) head(onlineResponseTimes)
-
-m_onl <- mean(onlineResponseTimes$deltaTms)
-s_onl <- sd(onlineResponseTimes$deltaTms)
-N_onl <- length(onlineResponseTimes$deltaTms)
-e_onl <- qnorm(0.025, lower.tail = FALSE * s_onl / sqrt(N_onl))
-lower_onlineDeltaT <- m_onl - e_onl
-upper_onlineDeltaT <- m_onl + e_onl
-if(debug) {
-  cat('\nOnline Command Response DeltaT Results')
-  cat('\nMean  = ', m_onl)
-  cat('\nSd    = ', s_onl)
-  cat('\nN     = ', N_onl)
-  cat('\nError = ', e_onl)
-  cat('\nLower = ', lower_onlineDeltaT)
-  cat('\nUpper = ', upper_onlineDeltaT)
-}
+  select(PacketNo, deltaTms) %>%
+  filter(deltaTms <= 1000)
+if(debug) dataDump(onlineResponseTimes$deltaTms)
 
 onlineResponseTimes %>%
   ggplot(aes(x=deltaTms)) +
   geom_histogram(binwidth = 0.01, color = 'darkgreen', fill = 'lightgreen') + 
-  coord_cartesian(xlim = c(lower_onlineDeltaT, upper_onlineDeltaT)) + 
+  coord_cartesian(xlim = c(0,5)) + 
   labs(x = '[ms]',
        y = '',
        title = 'Response Time Distribution',
